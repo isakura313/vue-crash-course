a6a2ac9bff6bc5cfb67d68726bf2144416c9fd83
Pavel <isakura313@gmail.com>
2020-08-14 18:16:45 +0300

    просто начало программирования на  VUE

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,29 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+<h1> ToDo Application</h1>
+    <hr>
+<TodoList
+v-bind:todos="todos"
+/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
+import TodoList from '@/components/TodoList'
 export default {
   name: 'App',
+  data(){
+  return{
+    todos:[
+      {id: 1, title: "Купить Хлеб", completed: false},
+      {id: 2, title: "Купить пиво", completed: false},
+      {id: 3, title: "Купить грибы", completed: true},
+    ]
+  }
+  },
   components: {
-    HelloWorld
+  TodoList
   }
 }
 </script>
